87ec5851e2424e1d40e82e1b8033343fe8dfb63c
wenlincheng <wen_lincheng@163.com>
2021-02-06 14:02:59 +0800

    整合seata

--- docker-compose/conf/seata/seata.sql
+++ docker-compose/conf/seata/seata.sql
@@ -4,52 +4,52 @@
 -- the table to store GlobalSession data
 drop table if exists `global_table`;
 create table `global_table` (
-`xid` varchar(128) not null,
-`transaction_id` bigint,
-`status` tinyint not null,
-`application_id` varchar(32),
-`transaction_service_group` varchar(32),
-`transaction_name` varchar(128),
-`timeout` int,
-`begin_time` bigint,
-`application_data` varchar(2000),
-`gmt_create` datetime,
-`gmt_modified` datetime,
-primary key (`xid`),
-key `idx_gmt_modified_status` (`gmt_modified`, `status`),
-key `idx_transaction_id` (`transaction_id`)
+    `xid` varchar(128) not null,
+    `transaction_id` bigint,
+    `status` tinyint not null,
+    `application_id` varchar(32),
+    `transaction_service_group` varchar(32),
+    `transaction_name` varchar(128),
+    `timeout` int,
+    `begin_time` bigint,
+    `application_data` varchar(2000),
+    `gmt_create` datetime,
+    `gmt_modified` datetime,
+    primary key (`xid`),
+    key `idx_gmt_modified_status` (`gmt_modified`, `status`),
+    key `idx_transaction_id` (`transaction_id`)
 );
 -- the table to store BranchSession data
 drop table if exists `branch_table`;
 create table `branch_table` (
-`branch_id` bigint not null,
-`xid` varchar(128) not null,
-`transaction_id` bigint ,
-`resource_group_id` varchar(32),
-`resource_id` varchar(256) ,
-`lock_key` varchar(128) ,
-`branch_type` varchar(8) ,
-`status` tinyint,
-`client_id` varchar(64),
-`application_data` varchar(2000),
-`gmt_create` datetime,
-`gmt_modified` datetime,
-primary key (`branch_id`),
-key `idx_xid` (`xid`)
+    `branch_id` bigint not null,
+    `xid` varchar(128) not null,
+    `transaction_id` bigint ,
+    `resource_group_id` varchar(32),
+    `resource_id` varchar(256) ,
+    `lock_key` varchar(128) ,
+    `branch_type` varchar(8) ,
+    `status` tinyint,
+    `client_id` varchar(64),
+    `application_data` varchar(2000),
+    `gmt_create` datetime,
+    `gmt_modified` datetime,
+    primary key (`branch_id`),
+    key `idx_xid` (`xid`)
 );
 -- the table to store lock data
 drop table if exists `lock_table`;
 create table `lock_table` (
-`row_key` varchar(128) not null,
-`xid` varchar(96),
-`transaction_id` long ,
-`branch_id` long,
-`resource_id` varchar(256) ,
-`table_name` varchar(32) ,
-`pk` varchar(36) ,
-`gmt_create` datetime ,
-`gmt_modified` datetime,
-primary key(`row_key`)
+    `row_key` varchar(128) not null,
+    `xid` varchar(96),
+    `transaction_id` long ,
+    `branch_id` long,
+    `resource_id` varchar(256) ,
+    `table_name` varchar(32) ,
+    `pk` varchar(36) ,
+    `gmt_create` datetime ,
+    `gmt_modified` datetime,
+    primary key(`row_key`)
 );
 
 -- the table to store seata xid data
@@ -57,17 +57,17 @@ primary key(`row_key`)
 -- you must to init this sql for you business databese. the seata server not need it.
 -- 此脚本必须初始化在你当前的业务数据库中，用于AT 模式XID记录。与server端无关（注：业务数据库）
 -- 注意此处0.3.0+ 增加唯一索引 ux_undo_log
-drop table `undo_log`;
+drop table if exists `undo_log`;
 CREATE TABLE `undo_log` (
-`id` bigint(20) NOT NULL AUTO_INCREMENT,
-`branch_id` bigint(20) NOT NULL,
-`xid` varchar(100) NOT NULL,
-`context` varchar(128) NOT NULL,
-`rollback_info` longblob NOT NULL,
-`log_status` int(11) NOT NULL,
-`log_created` datetime NOT NULL,
-`log_modified` datetime NOT NULL,
-`ext` varchar(100) DEFAULT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `branch_id` bigint(20) NOT NULL,
+    `xid` varchar(100) NOT NULL,
+    `context` varchar(128) NOT NULL,
+    `rollback_info` longblob NOT NULL,
+    `log_status` int(11) NOT NULL,
+    `log_created` datetime NOT NULL,
+    `log_modified` datetime NOT NULL,
+    `ext` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
