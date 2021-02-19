325d549a104e71431633c7c8bf9332d8c6130833
wenlincheng <wenlcmax@gmail.com>
2021-02-19 16:11:41 +0800

    item app

---
+++ pika-item/pika-item-app/src/main/java/com/wenlincheng/pika/item/controller/SaleSkuController.java
@@ -0,0 +1,21 @@
+package com.wenlincheng.pika.item.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import com.wenlincheng.pika.common.core.base.controller.BaseController;
+
+/**
+ * <p>
+ * 销售SKU 前端控制器
+ * </p>
+ *
+ * @author Pikaman
+ * @since 2021-01-31
+ */
+@RestController
+@RequestMapping("/app/sale-sku")
+public class SaleSkuController extends BaseController {
+
+}
