91790677fa90ef95d01e61a250e7347911e3590a
wenlincheng <wenlcmax@gmail.com>
2021-03-26 18:07:18 +0800

    整理import

--- pika-auth/src/main/java/com/wenlincheng/pika/auth/PikaAuthApplication.java
+++ pika-auth/src/main/java/com/wenlincheng/pika/auth/PikaAuthApplication.java
@@ -1,14 +1,11 @@
 package com.wenlincheng.pika.auth;
 
-import com.wenlincheng.pika.auth.config.AuthIgnoredUrisProperties;
 import com.wenlincheng.pika.auth.utils.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
